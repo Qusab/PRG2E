b0eef1a7a627ce38435505530166fcfed9221988
bohdan.fomin <bohdan.fomin@infis.cz>
2023-12-15 08:43:36 +0100

    procvicka

--- src/TESTSTRING/TESSSSSSSSS.java
+++ src/TESTSTRING/TESSSSSSSSS.java
@@ -3,17 +3,17 @@ import java.util.HashMap;
 
 public class TESSSSSSSSS {
     public static void main(String[] args) {
-        String[]words={"msmodisvmkcmvsgs*adjadjaf"};
-        int wordCount = 0;
-        boolean inWord = false;
-        for (char c : words.toCharArray()) {
-            if (Character.isWhitespace(c)) {
-                inWord = false;
-            } else if (!inWord) {
-                inWord = true;
-                wordCount++;
-            }
-        }
-        System.out.println(wordCount);
+//        String[]words={"msmodisvmkcmvsgs*adjadjaf"};
+//        int wordCount = 0;
+//        boolean inWord = false;
+//        for (char c : words.toCharArray()) {
+//            if (Character.isWhitespace(c)) {
+//                inWord = false;
+//            } else if (!inWord) {
+//                inWord = true;
+//                wordCount++;
+//            }
+//        }
+//        System.out.println(wordCount);
     }
 }
--- src/TESTSTRING/TESsSS.java
+++ src/TESTSTRING/TESsSS.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 public class TESsSS {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        String input=sc.nextLine();
+
         System.out.println("Zadaj heslo");
+        String input=sc.nextLine();
         for (int i = 0; i <input.length(); i++) {
             if(input.length()>8) {
                 System.out.println("delka je ok");
-            }                if (input.contains("H")) {
+            }                if (input.charAt(0) !='H') {
                 System.out.println("Zacina pismenem 'H'");
             }
                     if (input.contains("*")){
-                        System.out.println("Mas znak '*'");
+                        System.out.println("Mas znak '*'"+"  Super- je spravně");
+                        break;
                     }
             System.out.println("Zkus znovu");
                 }
