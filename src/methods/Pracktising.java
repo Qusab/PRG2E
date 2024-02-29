73a96ee72c598116d35bc2170367cff073203022
bohdan.fomin <bohdan.fomin@infis.cz>
2024-02-29 12:21:11 +0100

    pogjsoigdsogjsv

---
+++ src/methods/Pracktising.java
@@ -0,0 +1,28 @@
+package methods;
+
+
+public class Pracktising {
+    //pokud dva  cisla a jedno z nich zaporny
+    static int randomNubmers(int min, int max) {
+        //bud tak
+        int random = (int) (Math.random()*(2*max+1)-(-min) );
+         return  random;
+         //anebo
+        //return (int)(Math.random()*(max-min + 1)+ min)
+
+    }
+    static int[] randomArray(int min,int max, int lenght){
+        int array = new int [lenght];
+        for (int i = 0; i <array.lenght; i++) {
+            array[i]= randomNubmers(min, max);
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            System.out.println(randomNubmers(-24,24));
+        }
+        int []array= randomArray(1,100,100)));
+    }
+}
